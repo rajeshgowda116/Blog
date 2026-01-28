f925d5688765cf7045e074bba340d91d31ebb127
Rajesh <rajeshpar2005@gmail.com>
2026-01-28 21:59:32 +0530

    i added auto generated slug

--- blogs/admin.py
+++ blogs/admin.py
@@ -1,5 +1,7 @@
 from django.contrib import admin
 from .models import Category, Blog
 # Register your models here.
+class BlogAdmin(admin.ModelAdmin):
+    prepopulated_fields = {'slug': ('title',)}
 admin.site.register(Category)
-admin.site.register(Blog)
+admin.site.register(Blog, BlogAdmin) 
