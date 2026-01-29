c8983f9be4da28a3f47b222cd458781082894b51
Rajesh <rajeshpar2005@gmail.com>
2026-01-29 21:55:34 +0530

    added admin table list display

--- blogs/admin.py
+++ blogs/admin.py
@@ -3,5 +3,6 @@ from .models import Category, Blog
 # Register your models here.
 class BlogAdmin(admin.ModelAdmin):
     prepopulated_fields = {'slug': ('title',)}
+    list_display = ('title', 'Category', 'author','is_featured')
 admin.site.register(Category)
 admin.site.register(Blog, BlogAdmin) 
